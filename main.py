9a0f9731bf218306de0bb59e7d373778bf3b1c06
lambd4 <3163083993@qq.com>
2025-07-11 21:50:26 +0800

    fix pre op in img

--- main.py
+++ main.py
@@ -206,8 +206,8 @@ def main(args):
 
     cudnn.benchmark = True
 
-    dataset_train, args.nb_classes = build_dataset(is_train=True, args=args)
-    dataset_val, _ = build_dataset(is_train=False, args=args)
+    dataset_train, args.nb_classes = build_dataset(args=args)
+    dataset_val, _ = build_dataset(args=args)
 
     if args.distributed:
         num_tasks = utils.get_world_size()
